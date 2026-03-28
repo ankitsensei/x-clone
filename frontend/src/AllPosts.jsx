ae8a2976eb4810aa9b5a7c264c9ba81c4608bdd1
Ankit Bhagat <ankitbhagat.dev@gmail.com>
2026-03-28 16:30:00 +0530

    basic ui of taking input

---
+++ frontend/src/AllPosts.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { BiPhotoAlbum } from "react-icons/bi";
+
+const Posts = () => {
+  return (
+    <div>
+      <div className="text-zinc-400 border-zinc-700 border-b p-2 flex justify-evenly items-center h-14">
+        <p className="text-zinc-100">For you</p>
+        <p>Following</p>
+      </div>
+      {/* Post */}
+      <div className="h-full pt-2 px-4 flex gap-2">
+        <img
+          src="https://i.pinimg.com/736x/87/5b/4f/875b4fb82c44a038466807b0dcf884cc.jpg"
+          alt="dp"
+          className="w-10 h-10 rounded-full mt-2"
+        />
+        <div className="w-full h-fit pr-2">
+          <input
+            type="text"
+            placeholder="What's happening?"
+            className="mt-4 text-lg outline-none h-fit w-full"
+          />
+          <div className="w-full border-t border-zinc-700 mt-10 flex py-4 items-center justify-between">
+            <BiPhotoAlbum className="w-5 h-5 text-blue-400" />
+            <button className="px-4 py-1 font-semibold bg-zinc-400 text-black rounded-full hover:bg-zinc-200">
+              Post
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="flex gap-3 items-center p-2 border-t border-zinc-700">
+        <img
+          src="https://i.pinimg.com/736x/87/5b/4f/875b4fb82c44a038466807b0dcf884cc.jpg"
+          alt="dp"
+          className="w-10 rounded-full"
+        />
+        <div className="flex gap-2">
+          <p className="text-sm font-bold">Ankit Bhagat</p>
+          <p className="text-sm font-light text-zinc-400">@webdevankit</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Posts;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import AllPosts from "./AllPosts";
 
 const App = () => {
   return (
-    <div className="px-60 w-full">
-      <Navbar />
-      <div></div>
-      <div></div>
+    <div className="px-60 w-full flex items-start">
+      <div className="w-1/5 border-r border-zinc-800 h-screen">
+        <Navbar />
+      </div>
+      <div className="w-3/6 border-s border-zinc-800 h-screen">
+        <AllPosts />
+      </div>
+      <div className="w-1/5 border-s border-zinc-800 h-screen"></div>
     </div>
   );
 };
