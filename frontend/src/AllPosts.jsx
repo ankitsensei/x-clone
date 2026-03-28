d0330b43104db4f37e0592646694a4bc5e4221c3
Ankit Bhagat <ankitbhagat.dev@gmail.com>
2026-03-28 23:59:47 +0530

    gap

--- frontend/src/AllPosts.jsx
+++ frontend/src/AllPosts.jsx
@@ -82,9 +82,13 @@ const Posts = () => {
                   </p>
                 </div>
               </div>
-              <div className="px-13 -mt-5">
-                <p className="">{post.text}</p >
-                <img src={`data:image/jpeg;base64,${post.media}`} alt="#" className="w-full rounded-2xl" />
+              <div className="px-13 -mt-5 flex flex-col gap-2">
+                <p className="">{post.text}</p>
+                <img
+                  src={`data:image/jpeg;base64,${post.media}`}
+                  alt="#"
+                  className="w-full rounded-2xl"
+                />
               </div>
             </div>
           ))}
