c0405b616575951b3fe30daa48a62b33c2109fa9
Ankit Bhagat <ankitbhagat.dev@gmail.com>
2026-03-29 14:19:38 +0530

    fix: all post will show latest first

--- frontend/src/components/ShowAllPost.jsx
+++ frontend/src/components/ShowAllPost.jsx
@@ -27,34 +27,37 @@ const ShowAllPost = () => {
         <Spinner />
       ) : (
         <div>
-          {posts.map((post, index) => (
-            <div
-              key={index}
-              className="flex flex-col gap-3 items-start justify-center p-2 border-t border-zinc-700"
-            >
-              <div className="flex items-center gap-3">
-                <img
-                  src="https://i.pinimg.com/736x/87/5b/4f/875b4fb82c44a038466807b0dcf884cc.jpg"
-                  alt="dp"
-                  className="w-10 rounded-full"
-                />
-                <div className="flex gap-2">
-                  <p className="text-sm font-bold">Ankit Bhagat</p>
-                  <p className="text-sm font-light text-zinc-400">
-                    @webdevankit
-                  </p>
+          {posts
+            .slice()
+            .reverse()
+            .map((post, index) => (
+              <div
+                key={index}
+                className="flex flex-col gap-3 items-start justify-center p-2 border-t border-zinc-700"
+              >
+                <div className="flex items-center gap-3">
+                  <img
+                    src="https://i.pinimg.com/736x/87/5b/4f/875b4fb82c44a038466807b0dcf884cc.jpg"
+                    alt="dp"
+                    className="w-10 rounded-full"
+                  />
+                  <div className="flex gap-2">
+                    <p className="text-sm font-bold">Ankit Bhagat</p>
+                    <p className="text-sm font-light text-zinc-400">
+                      @webdevankit
+                    </p>
+                  </div>
+                </div>
+                <div className="px-13 -mt-5 flex flex-col gap-2">
+                  <p className="">{post.text}</p>
+                  <img
+                    src={`data:image/jpeg;base64,${post.media}`}
+                    alt="#"
+                    className="w-full rounded-2xl"
+                  />
                 </div>
               </div>
-              <div className="px-13 -mt-5 flex flex-col gap-2">
-                <p className="">{post.text}</p>
-                <img
-                  src={`data:image/jpeg;base64,${post.media}`}
-                  alt="#"
-                  className="w-full rounded-2xl"
-                />
-              </div>
-            </div>
-          ))}
+            ))}
         </div>
       )}
     </div>
