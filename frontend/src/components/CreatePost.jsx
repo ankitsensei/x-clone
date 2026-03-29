18e8d1d9e5dcc8252ad4f4ca556f861aed3103f5
Ankit Bhagat <ankitbhagat.dev@gmail.com>
2026-03-29 09:52:52 +0530

    feat: createpost working

--- frontend/src/components/CreatePost.jsx
+++ frontend/src/components/CreatePost.jsx
@@ -12,7 +12,7 @@ const CreatePost = () => {
   const handleSavePost = () => {
     const formData = new FormData();
     formData.append("text", text);
-    formData.append("photo", image);
+    formData.append("media", media);
     setLoading(true);
     axios
       .post("http://localhost:5555/", formData)
@@ -30,30 +30,56 @@ const CreatePost = () => {
     ref.current.style.height = "auto";
     ref.current.style.height = ref.current.scrollHeight + "px";
   };
+  console.log(mediaPreview);
   return (
     <div>
-      <div className="h-full pt-2 px-4 flex gap-2">
-        <img
-          src="https://i.pinimg.com/736x/87/5b/4f/875b4fb82c44a038466807b0dcf884cc.jpg"
-          alt="dp"
-          className="w-10 h-10 rounded-full mt-2"
-        />
-        <div className="w-full h-fit pr-2">
-          <textarea
-            ref={ref}
-            onInput={handleInput}
-            placeholder="What's happening?"
-            className="mt-4 text-lg outline-none w-full resize-none overflow-hidden"
-            rows={1}
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className="h-full w-full pt-2 px-4 flex gap-2">
+          <img
+            src="https://i.pinimg.com/736x/87/5b/4f/875b4fb82c44a038466807b0dcf884cc.jpg"
+            alt="dp"
+            className="w-10 h-10 rounded-full mt-2"
           />
-          <div className="w-full border-t border-zinc-700 mt-10 flex py-4 items-center justify-between">
-            <BiPhotoAlbum className="w-5 h-5 text-blue-400" />
-            <button className="px-4 py-1 font-semibold bg-zinc-400 text-black rounded-full hover:bg-zinc-200">
-              Post
-            </button>
+          <div className="w-full h-fit pr-2">
+            <textarea
+              type="text"
+              ref={ref}
+              onInput={handleInput}
+              placeholder="What's happening?"
+              className="mt-4 text-lg outline-none w-full resize-none overflow-hidden"
+              rows={1}
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+            <div className="flex flex-col justify-center items-center mt-4">
+              {mediaPreview && (
+                <img
+                  src={mediaPreview}
+                  alt="img"
+                  className="w-auto h-74 rounded-xl object-cover"
+                />
+              )}
+            </div>
+            <div className="w-full border-t border-zinc-700 mt-10 flex py-4 items-center justify-between">
+              <button>
+                <BiPhotoAlbum className="w-5 h-5 text-blue-400" />
+                <input
+                  type="file"
+                  onChange={(e) => setMedia(e.target.files[0])}
+                />
+              </button>
+              <button
+                onClick={() => handleSavePost()}
+                className="px-4 py-1 font-semibold bg-zinc-400 text-black rounded-full hover:bg-zinc-200"
+              >
+                Post
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
